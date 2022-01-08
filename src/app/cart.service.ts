ecb65141a80559d3d161bf80ae0c731e2e573ded
MisakiShimazaki <msk.toybox@gmail.com>
2022-01-08 19:34:56 +0900

    2 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -6,7 +7,7 @@ import { Product } from './products';
 })
 export class CartService {
   items: Product[] = [];
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   // 製品をカートに追加
   addToCart(product: Product) {
@@ -22,6 +23,12 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 
   // カートから削除する、とかいらんのか？

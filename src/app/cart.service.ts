defab25c936e59eb73d95c12e633dcf622a6c917
MisakiShimazaki <msk.toybox@gmail.com>
2022-01-08 17:48:31 +0900

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Product } from './products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  items: Product[] = [];
+  constructor() {}
 
-  constructor() { }
+  // 製品をカートに追加
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  // 商品リストを返す
+  getItems() {
+    return this.items;
+  }
+
+  // カートを空にする
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  // カートから削除する、とかいらんのか？
 }
